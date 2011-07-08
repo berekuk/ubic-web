5b6c3edb0e781aa4524da253e980ccd353560079
Simon Elliott <simon@papercreatures.com>
2011-07-08 15:59:32 +0100

    uri ideas

--- lib/Ubic/Web.pm
+++ lib/Ubic/Web.pm
@@ -1,20 +1,29 @@
 use Web::Simple 'Ubic::Web';
- 
+use JSON::XS;
+
 {
   package Ubic::Web;
   sub default_config {(
-    exclude => qw/service.a service.b.*/
+    exclude => [qw/service.a service.b.*/],
     # --or--
-    include => qw/service.a.*/
+    include => [qw/service.a.*/],
     servers => [{ 
-        host => '127.0.0.1:3456' 
-        exclude => qw/service.a service.b.*/
-        include => qw/service.a.*/
+        host => '127.0.0.1:3456',
+        exclude => [qw/service.a service.b.*/],
+        include => [qw/service.a.*/],
     }],
   )};
   sub dispatch_request {
     sub (/status ) {
-      [ 200, [ 'Content-type', 'text/plain' ], [ "Ok." ] ]
+      sub (/) {
+        [ 200, [ 'Content-type', 'text/plain' ], [ "Ok." ] ]
+      },
+      sub (/**) {
+        #multiservice subset i.e
+        # /status/web, /status/web/jobs
+        [ 200, [ 'Content-type', 'text/plain' ], [ "Ok." ] ]
+      },
+      
     }
   };
 }
