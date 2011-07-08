bbdba4364a5281f18d349e814b4688f9c8c6f691
Simon Elliott <simon@papercreatures.com>
2011-07-08 15:52:45 +0100

    config ideas

--- lib/Ubic/Web.pm
+++ lib/Ubic/Web.pm
@@ -2,11 +2,21 @@ use Web::Simple 'Ubic::Web';
  
 {
   package Ubic::Web;
+  sub default_config {(
+    exclude => qw/service.a service.b.*/
+    # --or--
+    include => qw/service.a.*/
+    servers => [{ 
+        host => '127.0.0.1:3456' 
+        exclude => qw/service.a service.b.*/
+        include => qw/service.a.*/
+    }],
+  )};
   sub dispatch_request {
     sub (/status ) {
       [ 200, [ 'Content-type', 'text/plain' ], [ "Ok." ] ]
     }
-  }
+  };
 }
  
 Ubic::Web->run_if_script;
